0cba019cfeb00ab224297f3a843ef108f317e4e5
Susan Quiros <squiros@houstonfoodbank.org>
2021-07-19 15:15:25 -0500

    working on the scrape_mars

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -33,6 +33,43 @@ def scrape():
     #JPL Mars space images
     url = 'https://spaceimages-mars.com/'
     browser.visit(url)
+     # HTML object
+    html = browser.html
+    # Parse HTML with Beautiful Soup
+    soup = BeautifulSoup(html, 'html.parser')
+    #creating the path for the image
+    image= browser.links.find_by_partial_text('FULL IMAGE').click()
+
+    # to keep connection with the website
+    html = browser.html
+    soup = BeautifulSoup(html,'html.parser')
+
+    featured_image_url = url + soup.find('img',class_='fancybox-image')['src']
+    print(f"Featured Image URL: {featured_image_url}")
+    #closing the browser 
+    browser.quit()  
+    #Mars facts
+    #inserting the url
+    url= 'https://galaxyfacts-mars.com/'
+    browser.visit(url)
+     # HTML object
+    html = browser.html
+    # Parse HTML with Beautiful Soup
+    soup = BeautifulSoup(html, 'html.parser')
+    mars_table= soup.find('table', class_="table table-striped")
+    mars_table= pd.read_html(str(mars_table))[0]
+    #mars_table   
+    mars_df = mars_table.rename(columns={ 0: "Characteristics", 1: "Facts"})
+    mars_df = mars_df.set_index('Characteristics') 
+    #converting df to html
+    mars_df_html= mars_df.to_html()
+    data_dict['table'] = mars_df
+    #Mars Hemispheres
+    
+
+
+
+
 
         
 
